f5960d500259504143776d2583c30fe3db748c3a
Johan Dahlin <johan.dahlin@newcastle.edu.au>
2017-09-27 16:58:17 +1000

    added helper for plotting.

--- stan/gaussianMixtureModelHeavyTailedData.py
+++ stan/gaussianMixtureModelHeavyTailedData.py
@@ -2,7 +2,7 @@ import numpy as np
 import pystan
 import matplotlib.pylab as plt
 from scipy.stats import norm
-from scipy.stats import gaussian_kde
+from helpers import plotting_helper
 
 noObservations = 1000
 observations = 5.0 + 2.0 * np.random.standard_t(df = 5.0, size=noObservations)
@@ -20,59 +20,5 @@ data = {'noObservations': noObservations,
         'gridPoints': gridPoints}
 
 sm = pystan.StanModel(file='gmmodel2.stan')
-fit = sm.sampling(data=data, iter=10000, chains=1)
-
-plt.figure(1)
-plt.subplot(3, 2, (1, 2))
-kernelDensityEstimator = gaussian_kde(observations)
-trueMixtureDensity = kernelDensityEstimator(gridPoints)
-estMixtureDensity = np.mean(np.exp(fit.extract("log_p_y_tilde")['log_p_y_tilde']), axis=0)
-plt.plot(gridPoints, trueMixtureDensity, 'b', gridPoints, estMixtureDensity ,'g')
-sns.despine(left=False, bottom=False, right=True)
-plt.xlabel("x")
-plt.ylabel("p(x)")
-
-plt.subplot(3, 2, 3)
-for i in range(5):
-        sns.distplot(fit.extract("mu")['mu'][:, i], color = Dark2_8.mpl_colors[i])
-sns.despine(left=False, bottom=False, right=True)
-plt.ylim(0, 10)
-plt.xlabel("mu")
-plt.ylabel("posterior estimate")
-
-plt.subplot(3, 2, 4)
-for i in range(5):
-        sns.distplot(fit.extract("sigma")['sigma'][:, i], color = Dark2_8.mpl_colors[i])
-sns.despine(left=False, bottom=False, right=True)
-plt.ylim(0, 10)
-plt.xlabel("sigma")
-plt.ylabel("posterior estimate")
-
-plt.subplot(3, 2, 5)
-sns.distplot(fit.extract("sigma0")['sigma0'], color = Dark2_8.mpl_colors[6])
-sns.despine(left=False, bottom=False, right=True)
-plt.xlabel("sigma0")
-plt.ylabel("posterior estimate")
-
-plt.subplot(3, 2, 6)
-sns.distplot(fit.extract("e0")['e0'], color = Dark2_8.mpl_colors[7])
-sns.despine(left=False, bottom=False, right=True)
-plt.xlabel("e0")
-plt.ylabel("posterior estimate")
-
-plt.show()
-
-
-plt.figure(2)
-for i in range(5):
-        plt.subplot(5, 2, 2*i + 1)
-        plt.plot(fit.extract("mu")['mu'][:, i], color = Dark2_8.mpl_colors[i])
-        plt.xlabel("mu")
-        plt.ylabel("Trace")
-        plt.subplot(5, 2, 2*i + 2)
-        plt.plot(fit.extract("sigma")['sigma'][:, i], color = Dark2_8.mpl_colors[i])
-        plt.xlabel("sigma")
-        plt.ylabel("Trace")
-fit.plot()
-
-#plt.show()
+fit = sm.sampling(data=data, iter=2000, chains=1)
+plotting_helper(fit)
--- stan/helpers.py
+++ stan/helpers.py
@@ -1,4 +1,8 @@
 import numpy as np
+import matplotlib.pylab as plt
+import seaborn as sns
+from palettable.colorbrewer.qualitative import Dark2_8
+
 
 def buildPhiMatrix(data, order):
     noObservations = len(data)
@@ -17,3 +21,55 @@ def randn_skew_fast(N, alpha=0.0, loc=0.0, scale=1.0):
     u1[u0 < 0] *= -1
     u1 = u1 + loc
     return u1
+
+def plotting_helper(fit):
+    plt.figure(1)
+    plt.subplot(3, 2, (1, 2))
+    kernelDensityEstimator = gaussian_kde(observations)
+    trueMixtureDensity = kernelDensityEstimator(gridPoints)
+    estMixtureDensity = np.mean(np.exp(fit.extract("log_p_y_tilde")['log_p_y_tilde']), axis=0)
+    plt.plot(gridPoints, trueMixtureDensity, 'b', gridPoints, estMixtureDensity ,'g')
+    sns.despine(left=False, bottom=False, right=True)
+    plt.xlabel("x")
+    plt.ylabel("p(x)")
+
+    plt.subplot(3, 2, 3)
+    for i in range(5):
+            sns.distplot(fit.extract("mu")['mu'][:, i], color = Dark2_8.mpl_colors[i])
+    sns.despine(left=False, bottom=False, right=True)
+    plt.ylim(0, 10)
+    plt.xlabel("mu")
+    plt.ylabel("posterior estimate")
+
+    plt.subplot(3, 2, 4)
+    for i in range(5):
+            sns.distplot(fit.extract("sigma")['sigma'][:, i], color = Dark2_8.mpl_colors[i])
+    sns.despine(left=False, bottom=False, right=True)
+    plt.ylim(0, 10)
+    plt.xlabel("sigma")
+    plt.ylabel("posterior estimate")
+
+    plt.subplot(3, 2, 5)
+    sns.distplot(fit.extract("sigma0")['sigma0'], color = Dark2_8.mpl_colors[6])
+    sns.despine(left=False, bottom=False, right=True)
+    plt.xlabel("sigma0")
+    plt.ylabel("posterior estimate")
+
+    plt.subplot(3, 2, 6)
+    sns.distplot(fit.extract("e0")['e0'], color = Dark2_8.mpl_colors[7])
+    sns.despine(left=False, bottom=False, right=True)
+    plt.xlabel("e0")
+    plt.ylabel("posterior estimate")
+    plt.show()
+
+    plt.figure(2)
+    for i in range(5):
+            plt.subplot(5, 2, 2*i + 1)
+            plt.plot(fit.extract("mu")['mu'][:, i], color = Dark2_8.mpl_colors[i])
+            plt.xlabel("mu")
+            plt.ylabel("Trace")
+            plt.subplot(5, 2, 2*i + 2)
+            plt.plot(fit.extract("sigma")['sigma'][:, i], color = Dark2_8.mpl_colors[i])
+            plt.xlabel("sigma")
+            plt.ylabel("Trace")
+    plt.show()
