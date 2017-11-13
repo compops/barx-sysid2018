e27f01e019fa8c7943736d53a959e57269018e48
Johan Dahlin <johan.dahlin@newcastle.edu.au>
2017-11-14 09:23:16 +1100

    Updated Dockerfile. It now works.
    Former-commit-id: acec6bcf083c955ca22d2f7393137e101bc6f7bf

--- Dockerfile
+++ Dockerfile
@@ -2,16 +2,18 @@
 FROM python:3.6
 
 # Set the working directory to /app
-WORKDIR /app
+WORKDIR /app/python
 
 # Copy the current directory contents into the container at /app
-ADD ./python /app
-ADD ./data /data
-ADD ./requirements.txt /app
+ADD ./python /app/python
+ADD ./data /app/data
+ADD ./requirements.txt /app/python
 
 # Install any needed packages specified in requirements.txt
-RUN pip install -r requirements.txt
+RUN pip install -r /app/python/requirements.txt
 
 # Run Python files when the container launches
-#RUN mkdir results
 CMD python run_script.py
+
+# Compress the results into one file
+CMD tar -zcvf /app/barx-sysid2018-results.tgz /app/results
--- run_script.py
+++ run_script.py
@@ -1,11 +1,21 @@
 """File for recreating experiments in paper."""
+import sys
 
 import python.illustration_gmms as illustration_gmms
 import python.example1_arx as example1_arx
 import python.example2_arxgmm as example2_arxgmm
 import python.example3_eegdata as example3_eegdata
 
-illustration_gmms.run()
-example1_arx.run()
-example2_arxgmm.run()
-example3_eegdata.run()
+if len(sys.argv) > 1:
+    if int(sys.argv[1]) == 0:
+        illustration_gmms.run()
+    elif int(sys.argv[1]) == 1:
+        example1_arx.run()
+    elif int(sys.argv[1]) == 2:
+        example2_arxgmm.run()
+    elif int(sys.argv[1]) == 3:
+        example3_eegdata.run()
+    else:
+        raise NameError("Unknown example to run...")
+else:
+    raise NameError("Need to supply an argument to function call (0, 1, 2, 3) correponding to the numerical illustration to run.")
